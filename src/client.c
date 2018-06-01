a404d7b6e244f5c5d3fef2f18818c6626b8d907e
caseyshields <caseyorigami@gmail.com>
2018-06-01 13:55:47 -0700

    Changing client to talk to the sensor over TCP

--- src/client.c
+++ src/client.c
@@ -1,28 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <winsock.h>
 #include <assert.h>
 #include <time.h>
 #include <sys/time.h>
 #include <sys/types.h>
+#include <unistd.h>
+#include <stdbool.h>
 #include "novasc3.1/novas.h"
 #include "tracker.h"
 #include "catalog.h"
 #include "vmath.h"
-#include <unistd.h>
-#include <stdbool.h>
+
+/** A tracker controller which issues TATS commands over ethernet, given star information.
+ * @author Casey Shields
+ */
 
 double get_time();
 char* get_arg( int argc, char *argv[], char *name, char* default_value );
-ssize_t get_input(char* prompt, char **line, size_t *size );
 
-void benchmark( Catalog* catalog, Tracker* tracker, int trials );
-
-/** a simple CLI interface for exercising various orion components. */
 int main( int argc, char *argv[] ) {
+    char *arg;
+
+    // tracker configuration
     double latitude, longitude, height;
     double celsius, millibars;
+
+    // Earth orientation/time configuration
     double ut1_utc, leap_secs;
-    char *arg, *path;
+
+    // socket communication
+    int sock; // socket descriptor
+    struct sockaddr_in server; // server address
+    unsigned short port; // port
+    char* ip; // in dotted quad notation
+    WSADATA wsadata; // winsock setup thing?
 
     // geodetic coordinates in degrees
     arg = get_arg( argc, argv, "-latitude", "38.88972222222222" );
@@ -51,8 +63,11 @@ int main( int argc, char *argv[] ) {
     arg = get_arg( argc, argv, "-leap_secs", "37.000000" );
     if( arg ) leap_secs = atof( arg );
 
-    // get the location of the catalog data
-    path = get_arg( argc, argv, "-catalog", "../data/FK6.txt");
+    // socket address
+    ip = get_arg( argc, argv, "-ip", "127.0.0.1");
+    arg = get_arg( argc, argv, "-port", "8080" );
+    if( arg ) port = atoi( arg );
+
 
     // create the tracker
     Tracker tracker;
@@ -67,9 +82,21 @@ int main( int argc, char *argv[] ) {
     setAtmosphere( &tracker, celsius, millibars );
     print_site( &tracker );
 
-    // create and load a catalog
-    FILE *file = fopen(path, "r");
-    Catalog* catalog = catalog_load_fk5(NULL, file);
+    // TODO the tracker should just a cat_entry structure as an argument rather than maintaining a catalog!...
+//    // create and load a catalog
+//    FILE *file = fopen(path, "r");
+//    Catalog* catalog = catalog_load_fk5(NULL, file);
+
+    // create the socket
+    if( WSAStartup( MAKEWORD(2, 0), wsadata) != 0 )
+        orion_exit( 1, "Failed to initialize winsock" );
+
+    if( (sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) )
+        orion_exit(1, "Failed to create socket" );
+
+    // currently using
+    //http://cs.ecs.baylor.edu/~donahoo/practical/CSockets/winsock.html
+    // for a simple example
 
     char *line = NULL;
     size_t size = 0 ;
@@ -78,131 +105,21 @@ int main( int argc, char *argv[] ) {
 
         // update the current time and print a prompt
         setTime( &tracker, get_time() );
-        printf( "orion[%lf]", getUTC(&tracker) );
-        read = get_input( "", &line, &size );
+        //printf( "orion[%lf]", getUTC(&tracker) );
 
-        // select a specific star by number
-        if( strncmp( "star", line, 4 ) == 0 ) {
-//            // filter by ID
-//            get_input("catalog number", &line, &size);
-//            int catalog_id = atoi(line);
-//            int check_id( Entry *entry ) {
-//                return entry->starnumber == catalog_id ? 1 : 0;
-//            }
-//            Catalog* results = catalog_filter(catalog, &check_id, NULL);
-
-            // filter by name
-            get_input("catalog number", &line, &size);
-            int check_name( Entry *entry ) {
-                return NULL != strstr(entry->novas.starname, line );
-            }
-            Catalog* results = catalog_filter(catalog, &check_name, NULL);
-
-            // transform each star to local coordinates
-            void process( Entry *entry ) {
-                double zd=0, az=0;
-                setTarget( &tracker, entry );
-                local( &tracker, &zd, &az );
-                entry_print( entry );
-                printf( "\tlocal : { zd:%lf, az:%lf}\n", zd, az );
-            }
-            catalog_each( results, process );
-
-            catalog_free(results);
-        }
-
-        // search within a lesser circle of the catalog
-        else if( strncmp( "dome", line, 4 )==0 ) {
-            get_input( "right ascension hours", &line, &size );
-            double ra = atof( line );//hours2radians( atof( line ) );
-
-            get_input( "declination degrees", &line, &size );
-            double dec = atof( line );//degrees2radians( atof( line ) );
-
-            get_input( "radius degrees", &line, &size );
-            double rad = atof( line );//degrees2radians( atof( line ) );
-
-            Catalog* results = catalog_search_dome(catalog, ra, dec, rad, NULL);
-            catalog_print(results);
-            printf( "\n%d stars found.\n", results->size );
-            catalog_free(results);
-        }
-
-        // search within a lesser circle of the catalog
-        else if( strncmp( "patch", line, 5 )==0 ) {
-            get_input( "minimum right ascension hours", &line, &size );
-            double ra_min = atof( line );
-
-            get_input( "maximum right ascension hours", &line, &size );
-            double ra_max = atof( line );
-
-            get_input( "minimum declination degrees", &line, &size );
-            double dec_min = atof( line );
-
-            get_input( "maximum declination degrees", &line, &size );
-            double dec_max = atof( line );
-
-            Catalog* results = catalog_search_patch(catalog, ra_min, ra_max, dec_min, dec_max, NULL);
-            catalog_print(results);
-            printf( "\n%d stars found.\n", results->size );
-            catalog_free(results);
-        }
-
-        // test an orange slice shaped section of sky
-        else if( strncmp("orange", line, 5)==0 ) {
-            get_input( "minimum right ascension hours", &line, &size );
-            double ra_min = atof( line );
-
-            get_input( "maximum right ascension hours", &line, &size );
-            double ra_max = atof( line );
-
-            Catalog* results = catalog_orange(catalog, ra_min, ra_max, NULL);
-            catalog_print(results);
-            printf( "\n%d stars found.\n", results->size );
-            catalog_free(results);
-        }
-
-        // figure out spherical celestial coordinates of the local zenith
-        else if( strncmp( "zenith", line, 6 )==0 ) {
-            double ra = 0, dec = 0;
-            zenith( &tracker, &ra, &dec );
-            printf( "Current zenith coodinates : (ra:%lf, dec:%lf)\n", ra, dec );
-        }
-
-        // print the entire catalog contents
-        else if( strncmp( "print", line, 5 )==0 ) {
-            catalog_print(catalog);
-        }
-
-        // run the benchmark
-        else if( strncmp( "bench", line, 5 )==0 ) {
-            benchmark( catalog, &tracker, 100 );
-        }
-
-        // clean up the program components and exit the program
-        else if( strncmp( "exit", line, 4 )==0 ) {
-            catalog_free_entries(catalog);
-            catalog_free(catalog);
-            free( line );
-            exit(0);
-        }
-
-        // print available commands
-        else {
-            printf( "Commands include \n\tstar\n\tzenith\n\tdome\n\tpatch\n\tprint\n\tbench\n\texit\n" );
-        }
     }
 }
 
-/** uses the GNU gettime of day method to get a accurate system time, then converts it to seconds since the unix epoch */
+/** Gets an accurate UTC timestamp from the system in seconds since the unix epoch */
 double get_time() {
     struct timeval time;
     gettimeofday( &time, NULL );
     return time.tv_sec + (time.tv_usec / 1000000.0);
 }
 
-/** retrieves the value subsequent to the specified option. If the default_value is supplied, the function will return
- * it if the option is not included. otherwise the method will print an error message and abort. */
+/** retrieves the value subsequent to the specified option. If the default_value
+ * is supplied, the function will return it. otherwise the method will print an
+ * error message and abort. */
 char* get_arg( int argc, char *argv[], char *name, char* default_value ) {
     for( int n=0; n<argc; n++ )
         if( !strcmp(name, argv[n]) )
@@ -211,60 +128,41 @@ char* get_arg( int argc, char *argv[], char *name, char* default_value ) {
         printf("Parameter '%s' defaulting to '%s'\n", name, default_value);
         return default_value;
     }
-    printf( "Error: missing parameter '%s'\n", name );
+    sprintf( "Error: missing parameter '%s'\n", name );
     exit( 1 );
 }
 
-/** Frees any data line is pointing to, then prompts the user, allocates a buffer, and reads the input.
- * Be sure to free the buffer after your last call to get_input! */
-ssize_t get_input(char* prompt, char **line, size_t *size ) {
-    if( *line ) {
-        free( *line );
-        *line = NULL;
-        *size = 0;
-    }
-    printf("%s : ", prompt);
-    fflush( stdout );
-    ssize_t read = getline( line, size, stdin );
-    (*line)[read-1] = '\0'; // trim trailing
+void orion_exit(int status, char* msg) {
 
-    if( read == -1 ) {
-        printf("Error: input stream closed");
-        exit( 2 );
-    }
-    return read;
+    if( msg!=NULL )
+        printf( msg );
+    exit( status );
+}
+// filter by name
+//get_input("catalog number", &line, &size);
+//int check_name( Entry *entry ) {
+//    return NULL != strstr(entry->novas.starname, line );
+//}
+//Catalog* results = catalog_filter(catalog, &check_name, NULL);
+
+// transform each star to local coordinates
+void process( Entry *entry ) {
+    double zd=0, az=0;
+    setTarget( &tracker, entry );
+    local( &tracker, &zd, &az );
+    entry_print( entry );
+    printf( "\tlocal : { zd:%lf, az:%lf}\n", zd, az );
 }
 
-/** Time how long it takes to point the tracker at every star in the catalog then prints the local coordinates. */
-void benchmark( Catalog* catalog, Tracker* tracker, int trials ) {
-    // start the timer
-    double start = get_time();
-    int size = catalog->size;
-
-    // track every star in the FK6 catalog
-    double tracks [catalog->size][2]; //double latitude, longitude;
-    for( int t=0; t<trials; t++ ) {
-        for (int n = 0; n < catalog->size; n++) {
-            Entry *entry = catalog->stars[n];
-            setTarget( tracker, entry );
-            local(tracker, &tracks[n][0], &tracks[n][1]);
-        }
-    }
-
-    // get the time
-    double end = get_time();
-    double duration = end - start;
-
-    // print the catalog with corresponding tracks
-    for( int n=0; n<catalog->size; n++ ) {
-        Entry* entry = catalog->stars[n];
-        entry_print(entry);
-        printf( "Horizon : (zd:%lf, az:%lf)\n\n", tracks[n][0], tracks[n][1] );
-    }
-
-    printf( "stars: %d\ntrials: %d\ntime: %lf\nspeed: %lf\n\n", catalog->size, trials, duration, duration/(trials*catalog->size) );
+void zenith() {
+    double ra = 0, dec = 0;
+    zenith( &tracker, &ra, &dec );
+    printf( "Current zenith coodinates : (ra:%lf, dec:%lf)\n", ra, dec );
 }
 
-void search_dome() {
-
+void close() {
+    catalog_free_entries(catalog);
+    catalog_free(catalog);
+    free( line );
+    exit(0);
 }
