abadf6401ad19eed8eb0a9c11148910ab1cc8d70
caseyshields <caseyorigami@gmail.com>
2018-07-12 13:41:27 -0700

    wrong place

--- src/h/fk6.h
+++ src/h/fk6.h
@@ -15,8 +15,7 @@
 #define SEPARATOR "--------------------------------------------------------------------------------\n"
 //define FK6_3_FIELDS "   Bytes Format Units    Label     Explanations\n"
 
-/** A field to hold the metadata contained in Visier/CDS ReadMe files. Each field describes a
- * parameter in an ascii line of data. */
+/** Holds metadata for a field in a CDS VizieR ReadMe files. Each field describes a parameter in an ascii line of data. */
 typedef struct {
     /** The starting index of the field, 1-indexed, inclusive. */
     int start;
@@ -38,7 +37,7 @@ typedef struct {
     char Explanations[100];
 } FK6_Field;
 
-/** A collection of FK6_Fields which describe a CDS record, allowing  */
+/** Describes a CDS VizieR record. It is used to parse and convert values from a VizieR catalog record. */
 typedef struct {
     size_t cols;
     FK6_Field * fields;
@@ -51,21 +50,29 @@ FK6 * fk6_create();
 /** Scans to the given header in a CDS readme file, then loads the metadata in that section of the file. */
 int fk6_load_fields( FK6* fk6, FILE* readme, const char* header );
 
-/** Adds the given field to the record metadata */
+/** Adds the given field to the record metadata
+ * @param fk6 CDS VizieR metadata structure
+ * @param field */
 void fk6_add_field( FK6 * fk6, FK6_Field * field );
 
-/** @returns A pointer to the first field with the given name, or NULL if no such field exists */
+/** Retrieves field with the given name
+ * @param fk6 CDS VizieR metadata structure
+ * @param label name found in the 'Label' field of the 'ReadMe' metadata
+ * @returns A pointer to the first field with the given name, or NULL if no such field exists */
 FK6_Field * fk6_get_field( FK6 * fk6, const char * label );
 
-/** @return A pointer to the field at the given index. */
+/** Retrieves field data at the given index
+ * @param fk6 CDS VizieR metadata structure
+ * @param index
+ * @return A pointer to the field at the given index. */
 FK6_Field * fk6_get_index( FK6 * fk6, const int index );
 
 /** Uses the given metadata to parse the field from the record and convert it to the appropriate type.
  * Fields with a format of 'A' are converted to C strings. 'I' fields are converted to long integers.
  * 'F' fields are converted to doubles. Make sure the destination pinter is appropriately typed!
  * @param record A line of text from a Vizier/CDS record
- * @param the metadata of the desired field
- * @param a ponter whose value will be set to the value of the requested field.*/
+ * @param field the metadata of the desired field
+ * @param dest a ponter whose value will be set to the value of the requested field.*/
 int fk6_get_value( char * record, FK6_Field * field, void * dest );
 
 /** Frees the metadata information. */
